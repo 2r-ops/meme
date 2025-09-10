4509bd7059ae556d93aa9b782d6d7c26ec0788cf
2r-ops <rohitrishit141@gmail.com>
2025-09-10 09:46:23 +0530

    IMAGE REMOVED

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Meme loader
 function loadMeme() {
-  const totalMemes = 18;  // number of memes
+  const totalMemes = 16;  // number of memes
   const randomIndex = Math.floor(Math.random() * totalMemes) + 1;
   document.getElementById("meme").src = "meme/" + randomIndex + ".jpg";
 }
